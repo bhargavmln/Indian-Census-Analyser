fee3f183053ae4a0c2660a561beb2d8b47876c32
bhargavmln <bhargavmln@gmail.com>
2020-10-26 00:32:31 +0530

    [bhargavmln] TC1.2 :check if custom exception is returned if incorrect file

--- src/test/java/com/capg/censusanalyser/StateCensusAnalyserTest.java
+++ src/test/java/com/capg/censusanalyser/StateCensusAnalyserTest.java
@@ -1,13 +1,15 @@
 package com.capg.censusanalyser;
 
 import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 public class StateCensusAnalyserTest {
 
 	private static final String STATE_CENSUS_CSV_FILE_PATH = "./src/test/resources/StateCensusData.csv";
-
+	private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/StateCensusData.csv";
+	
 	@Test
 	public void givenCsvPath_ShouldReturn_NumberOfRecords() {
 		try {
@@ -17,6 +19,18 @@ public class StateCensusAnalyserTest {
 			System.out.println(numOfRecords);
 		} catch (CensusAnalyserException e) {
 			e.getMessage();
+		}
+	}
+
+	@Test
+	public void givenWrongFile_ShouldThrow_CustomException() {
+		try {
+			StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
+			ExpectedException exceptionRule = ExpectedException.none();
+			exceptionRule.expect(CensusAnalyserException.class);
+			stateCensusAnalyser.loadIndiaCensusData(WRONG_CSV_FILE_PATH);
+		} catch (CensusAnalyserException e) {
+			assertEquals(CensusAnalyserException.ExceptionType.FILE_NOT_FOUND, e.type);
 		}
 	}
 }
