c2fb1cf0ca9b024124f7329f4d0078ce66958598
bhargavmln <bhargavmln@gmail.com>
2020-10-26 16:16:28 +0530

    [bhargavmln] TC2.3 : check for exception when incorrect type

--- src/test/java/com/capg/censusanalyser/StateCensusAnalyserTest.java
+++ src/test/java/com/capg/censusanalyser/StateCensusAnalyserTest.java
@@ -13,6 +13,7 @@ public class StateCensusAnalyserTest {
 	private static final String INDIAN_CENSUS_CSV_WRONG_DELIMITER = "./src/test/resources/WrongDelimiterData.csv";
 	private static final String STATE_CODE_DATA = "./src/test/resources/StateCodeData.csv";
 	private static final String WRONG_FILE_PATH = "./src/main/resources/StateCensusData.csv";
+	private static final String CSV_WRONG_DATA = "./src/test/resources/WrongData.csv";
 
 	@Test
 	public void givenCsvPath_ShouldReturn_NumberOfRecords() {
@@ -90,6 +91,18 @@ public class StateCensusAnalyserTest {
 			stateCensusAnalyser.loadIndianStateCode(WRONG_FILE_PATH);
 		} catch (CensusAnalyserException e) {
 			assertEquals(CensusAnalyserException.ExceptionType.FILE_NOT_FOUND, e.type);
+		}
+	}
+	
+	@Test
+	public void givenRightStateCsvFile_ButWrongType_InStateCodeData_ShouldThrow_CustomException() {
+		try {
+			StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
+			ExpectedException exceptionRule = ExpectedException.none();
+			exceptionRule.expect(CensusAnalyserException.class);
+			int numOfRecords = stateCensusAnalyser.loadIndiaCensusData(CSV_WRONG_DATA);
+		} catch (CensusAnalyserException e) {
+			assertEquals(CensusAnalyserException.ExceptionType.CSV_FILE_INTERNAL_ISSUES, e.type);
 		}
 	}
 
